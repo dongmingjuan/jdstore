eae668b4ca046f6551f93cbddf94bd8a58d878dc
dongmingjuan <d387918156@gmail.com>
2017-05-04 16:59:40 +0800

    change the pages index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     resources :products
   end
   # root 'products#index'
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'pages#index'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
