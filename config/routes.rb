915d9c66747ea9ee6ff6e54a928a24e66e3ed73a
dongmingjuan <d387918156@gmail.com>
2017-05-07 09:39:18 +0800

    the model i need

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
     resources :products
   end
   # root 'products#index'
-  # root 'welcome#index'
+  root 'welcome#index'
   # root 'pages#index'
-  root 'pageones#index'
+  # root 'pageones#index'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
